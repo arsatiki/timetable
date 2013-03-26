eed914ec53c22e3a8708f3fe3c249a4ae0393dee
Antti Rasinen <antti.rasinen@codento.com>
2013-03-26 16:35:35 +0200

    Broken transitions

--- main.js
+++ main.js
@@ -4,9 +4,16 @@ function updateClock() {
 	clock.innerText = now.getHours() + ":" + now.getMinutes();
 }
 
+function killFirst() {
+	var u = document.getElementsByTagName('ul')[0];
+	var p = u.getElementsByTagName('li')[0];
+	u.removeChild(p);
+}
+
 window.onload = function() {
 	// document.getElementsByTagName('li')[0].className="disappear";
 	setInterval(updateClock, 60e3);
 	updateClock();
-}
 
+	document.getElementById('clock').addEventListener('click', killFirst);
+}
